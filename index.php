478d6af13a8115dc66288e820a8eccb4382ece8d
segnalazioni <segnalazionilavoro@gmail.com>
2016-10-14 01:08:48 +0200

    mh

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ session_start();
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <script src="bower_components/webcomponentsjs/webcomponents.js"></script>
+        <script src="bower_components/webcomponentsjs/webcomponents-lite.min.js"></script>
 
         <link rel="stylesheet" type="text/css" href="style.css"/>
     	<link rel="import" href="bower_components/paper-button/paper-button.html">
