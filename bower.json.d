712d60866ec973194c95304fa5e4bde6e839c9a1
segnalazioni <segnalazionilavoro@gmail.com>
2016-10-14 04:30:41 +0200

    14/10/2016 04:30:26

---
+++ bower.json.d
@@ -0,0 +1,59 @@
+{
+  "name": "segnalazioni",
+  "authors": [
+    "Alessandro <bipolar@hotmail.it>"
+  ],
+  "description": "segnalazioni per polizia",
+  "main": "index.php",
+  "license": "MIT",
+  "homepage": "https://github.com/segnalazioni/segnalazionipolaa",
+  "private": true,
+  "ignore": [
+    "**/.*",
+    "node_modules",
+    "bower_components",
+    "test",
+    "tests"
+  ],
+  "dependencies": {
+    "paper-dialog": "PolymerElements/paper-dialog#1.0.0",
+    "jquery": "^3.1.0",
+    "paper-dialog-behavior": "PolymerElements/paper-dialog-behavior#^1.2.7",
+    "paper-toolbar": "PolymerElements/paper-toolbar#^1.1.6",
+    "paper-tabs": "PolymerElements/paper-tabs#^1.6.2",
+    "paper-fab": "PolymerElements/paper-fab#^1.2.0",
+    "google-map": "GoogleWebComponents/google-map#1.0.0",
+    "iron-a11y-keys-behavior": "PolymerElements/iron-a11y-keys-behavior#^1.1.9",
+    "iron-behaviors": "PolymerElements/iron-behaviors#^1.0.17",
+    "iron-flex-layout": "PolymerElements/iron-flex-layout#^1.3.1",
+    "iron-form": "PolymerElements/iron-form#^1.1.4",
+    "iron-form-element-behavior": "PolymerElements/iron-form-element-behavior#^1.0.6",
+    "iron-icon": "PolymerElements/iron-icon#^1.0.11",
+    "iron-icons": "PolymerElements/iron-icons#1.0.0",
+    "iron-iconset-svg": "PolymerElements/iron-iconset-svg#1.0.0",
+    "iron-input": "PolymerElements/iron-input#1.0.0",
+    "iron-overlay-behavior": "PolymerElements/iron-overlay-behavior#1.0.0",
+    "paper-button": "PolymerElements/paper-button#1.0.0",
+    "paper-card": "PolymerElements/paper-card#1.0.0",
+    "paper-icon-button": "PolymerElements/paper-icon-button#1.0.0",
+    "paper-styles": "PolymerElements/paper-styles#1.0.0",
+    "paper-input": "PolymerElements/paper-input#^1.1.20"
+  },
+  "resolutions": {
+    "iron-selector": "^3.1.0",
+    "polymer": "^1.6.0",
+    "paper-styles": "^1.1.4",
+    "iron-form": "^1.1.4",
+    "iron-overlay-behavior": "^1.8.6",
+    "iron-input": "^1.0.8",
+    "paper-button": "^1.2.1",
+    "paper-icon-button": "^1.0.0",
+    "iron-icons": "^1.1.2"
+  },
+  "keywords": [
+    "segnalazioni",
+    "polizia",
+    "buche",
+    "incidenti"
+  ]
+}
