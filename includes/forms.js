1d5f3de439e07a0109f35f0e7a41cf13a5df7b52
segnalazioni <segnalazionilavoro@gmail.com>
2016-10-14 04:17:46 +0200

    14/10/2016 04:17:44

--- includes/forms.js
+++ includes/forms.js
@@ -1,5 +1,6 @@
 function formhash(user, password) {
     // Create a new element input, this will be our hashed password field.
+    alert("passing");
 	if(user.value != "" && password.value != ""){
 		var pas = hex_sha512(password.value);
 		var use = user.value;
