52b4279e537d07d533b469561f24ff0fdb6116b4
segnalazioni <segnalazionilavoro@gmail.com>
2016-10-14 04:11:56 +0200

    14/10/2016 04:11:55

--- includes/forms.js
+++ includes/forms.js
@@ -9,7 +9,7 @@ function formhash(user, password) {
 			type: 'POST',
 			data: {'p': pas, 'user': use},
 			success: function(data){
-			    alert(data);
+			    alert(data+"is here");
                 if(data == "user"){
                     document.getElementById('modal-user').open();
                 }else if(data == "pass"){
