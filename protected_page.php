fd1a8e71bf84a7bf9b36a1cf846ca420e15d5813
segnalazioni <segnalazionilavoro@gmail.com>
2016-10-27 05:15:00 +0200

    a

--- protected_page.php
+++ protected_page.php
@@ -322,16 +322,18 @@ include_once 'includes/functions.php';
                     	<div id="map" style="height:100%"></div>
         				<paper-fab icon="add" onclick="openDialog();" style="position:absolute; bottom:30px; right:30px; background-color:#e1382d;"></paper-fab>
                         <paper-dialog id="modal-add" class="segdialog" is="custom-style" modal>
-    						<div style="margin-top:0px; position:relative; padding:0; height:70%;">
-        						<div id="map-dialog" style="margin:0; padding:0; width:100%; height:100%;"></div>
-            					<my-menu admin="is"></my-menu>
-            					<paper-fab class="gpsfab" icon="device:gps-fixed" onclick="getShowLocation();" style="position:absolute; right:30px; --paper-fab-mini:true; bottom:-28px; background-color:#e1382d;"></paper-fab>
-            					<textarea id="description"></textarea>
-        					</div>
+                            <div>
+                                <div style="margin-top:0px; padding:0; height:70%;">
+                                    <div id="map-dialog" style="margin:0; padding:0; width:100%; height:100%;"></div>
+                                    <my-menu admin="is"></my-menu>
+                                    <paper-fab class="gpsfab" icon="device:gps-fixed" onclick="getShowLocation();" style="position:absolute; right:30px; --paper-fab-mini:true; bottom:-28px; background-color:#e1382d;"></paper-fab>
+                                    <textarea id="description"></textarea>
+                                </div>
 
-                            <div class="buttons" style="bottom:5px; right:15px;">
-                                <paper-button dialog-dismiss>ANNULLA</paper-button>
-                                <paper-button dialog-confirm autofocus onclick="addToDatabase();">SEGNALA</paper-button>
+                                <div class="buttons" style="bottom:5px; right:15px;">
+                                    <paper-button dialog-dismiss>ANNULLA</paper-button>
+                                    <paper-button dialog-confirm autofocus onclick="addToDatabase();">SEGNALA</paper-button>
+                                </div>
                             </div>
         				</paper-dialog>
                         <paper-dialog id="aggiorna-stato" modal>
