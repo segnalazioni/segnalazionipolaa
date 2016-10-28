0edffe0f9a8889d2010b7f1484c9cf7c964f6f83
segnalazioni <segnalazionilavoro@gmail.com>
2016-10-28 07:32:37 +0200

    aa

--- protected_page.php
+++ protected_page.php
@@ -332,7 +332,7 @@ include_once 'includes/functions.php';
                                                 <my-menu admin="is"></my-menu>
                                                 <paper-fab class="camerafab" icon="image:camera-alt" onclick="document.getElementById('getPhoto').click();"></paper-fab>
                                                 <paper-fab class="gpsfab" icon="device:gps-fixed" onclick="getShowLocation();" style="position:absolute; left:20px; --paper-fab-mini:true; bottom:-28px; background-color:#e1382d;"></paper-fab>
-                                                <input id="getPhoto" type="file" accept="image/*" capture="camera">
+                                                <input id="getPhoto" type="file" accept="image/*" capture="camera" hidden>
                                             </div>
                                         </td>
                                     </tr>
