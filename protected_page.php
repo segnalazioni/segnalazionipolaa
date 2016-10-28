49c6155c552f61cd7b9697fdf11c19e3928fa21a
segnalazioni <segnalazionilavoro@gmail.com>
2016-10-28 05:04:27 +0200

    aa

--- protected_page.php
+++ protected_page.php
@@ -329,7 +329,7 @@ include_once 'includes/functions.php';
                                         <td height="70%">
                                             <div style="width: 100%; height: 100%; position: relative;">
                                                 <div id="map-dialog" style="width: 100%; height: 100%;"></div>
-                                                <my-menu style="position:absolute; bottom:-30px; left: 20px;" admin="is"></my-menu>
+                                                <my-menu admin="is"></my-menu>
                                                 <paper-fab class="gpsfab" icon="image:camera-alt" style="position:absolute; right:100px; --paper-fab-mini:true; bottom:-28px; background-color:#e1382d;"><input type="file" accept="image/*" capture="camera"></paper-fab>
                                                 <paper-fab class="gpsfab" icon="device:gps-fixed" onclick="getShowLocation();" style="position:absolute; right:30px; --paper-fab-mini:true; bottom:-28px; background-color:#e1382d;"></paper-fab>
                                             </div>
