3b72379ec0cef39763911d62482c62c652db271a
segnalazioni <segnalazionilavoro@gmail.com>
2016-10-28 02:55:32 +0200

    aa

--- protected_page.php
+++ protected_page.php
@@ -324,7 +324,7 @@ include_once 'includes/functions.php';
                         <paper-dialog id="modal-add" class="segdialog" is="custom-style" modal>
                             <div id="mainDiv" style="margin: 0; padding: 0; height: 100%; width: 100%;">
                                 <table class="dialog-content" border="0" cellspacing="0" cellpadding="0" style="width: 100%;">
-                                    <tr>
+                                    <tr style="position: relative;">
                                         <td height="70%"><div id="map-dialog" style="width: 100%; height: 100%;"></div></td>
                                         <my-menu style="position:absolute; bottom:0; left: 20px;" admin="is"></my-menu>
                                     </tr>
