945aff7431b66eaa8ae7e1f110cffc88351ea977
segnalazioni <segnalazionilavoro@gmail.com>
2016-10-28 02:53:47 +0200

    aa

--- protected_page.php
+++ protected_page.php
@@ -325,7 +325,8 @@ include_once 'includes/functions.php';
                             <div id="mainDiv" style="margin: 0; padding: 0; height: 100%; width: 100%;">
                                 <table class="dialog-content" border="0" cellspacing="0" cellpadding="0" style="width: 100%;">
                                     <tr>
-                                        <td height="70%"><div id="map-dialog" style="width: 100%; height: 100%; position: relative;"><my-menu style="position:absolute; bottom:0; left: 20px;" admin="is"></my-menu></div></td>
+                                        <td height="70%"><div id="map-dialog" style="width: 100%; height: 100%;"></div></td>
+                                        <my-menu style="position:absolute; bottom:0; left: 20px;" admin="is"></my-menu>
                                     </tr>
                                     <tr>
                                         <td height="30%"><textarea style="resize:none; box-sizing: border-box; vertical-align: top; width:100%; height:100%;"></textarea></td>
