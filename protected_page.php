b9124a87797e797c0632cb548bc9346cdc93c086
segnalazioni <segnalazionilavoro@gmail.com>
2016-10-29 05:17:54 +0200

    aa

--- protected_page.php
+++ protected_page.php
@@ -218,7 +218,7 @@ include_once 'includes/functions.php';
 				function addToDatabase(){
 					var tipo = document.querySelector('my-menu').getSelectedElement();
                     var files = document.getElementById('getphoto').files;
-                    alert(files[0]+" file");
+                    alert(files[0].name+"");
 					var descrizione =  $('textarea').val();
 					if(latitude != null && longitude != null){
 						$.ajax ( {
