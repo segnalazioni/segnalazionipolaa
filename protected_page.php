92e240080d06ba0564bd50f0ea4f9c370adba915
segnalazioni <segnalazionilavoro@gmail.com>
2016-10-30 02:46:23 +0100

    aa

--- protected_page.php
+++ protected_page.php
@@ -87,6 +87,15 @@ include_once 'includes/functions.php';
 										title: result[i].tipo+"  "+result[i].quando
 									});
 									markers[id] = marker;
+                                    var app = document.querySelector('#scope');
+                                    app.data = [
+                                        {data: 20-08-1994, commento: 'Frozen yogurt'},
+                                        {data: 20-09-1940, commento: 'Ice cream sandwich'},
+                                        {data: 24-03-2015, commento: 'Eclair'},
+                                        {data: 10-01-2090, commento: 'Cupcake'},
+                                        {data: 01-12-2019, commento: 'Gingerbread'},
+                                        {data: 05-05-2005, commento: 'Jelly bean'}
+                                    ];
 									var contentString = '<div id="content">'+
 											'<div id="siteNotice">'+
 											  '</div>'+
