7def79483d616aa418cc22ea80be82510dc2e68e
segnalazioni <segnalazionilavoro@gmail.com>
2016-10-31 03:36:31 +0100

    a

--- protected_page.php
+++ protected_page.php
@@ -105,8 +105,8 @@ include_once 'includes/functions.php';
                                                 '</tr>'+
                                                 '</template>'+
                                                 '<tr class="second-row">'+
-												'<div style="float: right; right: 0;"><paper-button raised onclick="openUpdateDialog('+id+')">AGGIORNA STATO</paper-button>'+
-											  '<paper-button raised onclick="clickedclose('+id+', this)">'+button+'</paper-button>'+
+												'<div style="float: right; right: 0;"><paper-button raised class="custom white" onclick="openUpdateDialog('+id+')">AGGIORNA STATO</paper-button>'+
+											  '<paper-button raised class="custom indigo" onclick="clickedclose('+id+', this)">'+button+'</paper-button>'+
 											  '</div></tr></div>'+
 											  '</div>';
 									var infowindow = new google.maps.InfoWindow({
