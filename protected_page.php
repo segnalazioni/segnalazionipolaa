4c064e05378e31b147af30749ba3cc10a78163f0
segnalazioni <segnalazionilavoro@gmail.com>
2016-11-02 16:37:59 +0100

    a

--- protected_page.php
+++ protected_page.php
@@ -318,11 +318,11 @@ include_once 'includes/functions.php';
                 }
             </style>
 
-            <paper-toolbar style="padding: 0;">
+            <paper-toolbar style="padding: 0; width: 100%;">
                 <paper-tabs selected="{{selected}}" style="margin:0; width:100%; height:100%;">
-                    <paper-tab>MAPPA</paper-tab>
+                    <paper-tab style="margin: 0;">MAPPA</paper-tab>
                     <paper-tab>ELENCO</paper-tab>
-                    <paper-tab>REGISTRAZIONE</paper-tab>
+                    <paper-tab style="margin: 0;">REGISTRAZIONE</paper-tab>
                 </paper-tabs>
             </paper-toolbar>
 
